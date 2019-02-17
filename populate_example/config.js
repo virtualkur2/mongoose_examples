3516a17986b4f3eebcf04ed7aa1249528eec0084
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-02-17 21:40:37 +0100

    Delete db_usr and db_pwrd keys from populate_example/config.js

--- populate_example/config.js
+++ populate_example/config.js
@@ -2,8 +2,6 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/populate',
-  //dbUser: process.env.DB_USER || 'dev_usr-2018',
-  //dbPassword: process.env.DB_PASSWORD || 'dev@2018',
 }
 
 export default config;
